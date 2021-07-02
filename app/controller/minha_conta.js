caa294905dbe348351fc3a7e9a2e359b5f01b1d5
Marcos Schneider <marcos.schneider13@hotmail.com>
2021-07-02 17:45:51 -0300

    alteracoes envio de email ao desativar conta

--- app/controller/minha_conta.js
+++ app/controller/minha_conta.js
@@ -27,6 +27,34 @@ var usuarioParametrosAlgoritmoModel = require('../model/usuarioParametrosAlgorit
 var licencaModel = require('../model/licencaModel.js');
 var desativarContaModel = require('../model/desativarContaModel.js');
 
+
+
+
+var cabecalho_email = "<div style='background:#fff;background-color:#fff;margin:0px auto; max-width:600px;'>"+
+"<div style='background:#fff;width:100%;'>"+
+"<img style='width:100%;' src='https://einvestor.com.br/public/images/pdf/00.png'>"+
+"</div>"+
+"<div style='background:#f7f9fb;color:#000;width:100%;'>"+
+"<div style='padding:20px;font-size:13px;'>";
+
+var rodape_email = "<br><br><div><img style='max-width:250px;' src='https://einvestor.com.br/public/images/e_investor_logo.png'></div>"+
+"<br><br><span style='color:#666666;font-size:10px;'>A E-Investor é uma plataforma desenvolvida pela Innvicton Tecnologia, que desenvolve softwares e tecnologias quantitativas para usuários investidores. A Innvicton Tecnologia é uma empresa de desenvolvimento de software e não provê qualquer tipo de serviço de investimento ou corretagem nos mercados financeiros. Sistema desenvolvido exclusivamente para integração ao testador de estratégias da Metaquotes para geração de parâmetros de algoritmo pelo usuário com auxílio de Inteligência Artificial. Não atuamos como prestadora de informações de mercado, ou como receptora/transmissora de ordens de negociação para o sistema de distribuição de valores mobiliários. Esta plataforma, serviços e sistemas não constitui nem deve ser interpretado como oferta ou solicitação de compra ou venda de qualquer instrumento financeiro, ficando a decisão de investimento sempre a critério exclusivo do usuário com base em seu juízo de valor. O investidor deve consultar seu próprio assessor ou conselheiro jurídico, tributário, regulatório, técnico de negócios, de investimentos, financeiro e contábil, na medida que julgar necessário, para assessoria na escolha de definição de softwares, plataformas, sistemas, algoritmos, estratégias e setups. O investidor que realiza operações de renda variável é o único responsável pelas decisões de investimento ou de abstenção de investimento que tomar.</span>"+
+"<br><br><span style='color:#666666;font-size:10px;'>Os sistemas da E-Investor são totalmente parametrizáveis (White Box), contendo seus parâmetros abertos para preenchimento pelo usuário da maneira que ele preferir e julgar mais adequada, cabendo a cada usuário a tomada de decisão da estratégia ou setup que vai utilizar. Comercialização de estratégias automatizadas; CNAE 6202-3/00; Regulatório CVM referente a Instrução CVM n° 598/18 conforme Item 15 Do Ofício-Circular CVM/SIN 02/19. Copyright - Innvicton Tecnologia - Todos os direitos reservados.</span>"+
+"<br><br><span style='color:#666666;font-size:10px;'>Empresas e tecnologias as quais utilizamos ou recomendamos não tem nenhuma ligação ou participação em qualquer negócio, produto ou serviço da E-Investor, e não obstante não tem ligação com este site, sistema ou conteúdo de nenhuma maneira comercial ou explícita.</span>"+
+"<br><br><span style='color:#999999;font-size:10px;'>AVISO LEGAL </span>"+
+"<br><span style='color:#999999;font-size:10px;'>Esta mensagem é destinada exclusivamente para a(s) pessoa(s) a quem é dirigida, podendo conter informação confidencial e/ou legalmente privilegiada. Desde já fica notificado de abster-se a divulgar, copiar, distribuir, ou, de qualquer forma, utilizar a informação contida nesta mensagem, por ser ilegal. Caso você tenha recebido esta mensagem por engano, pedimos que nos retorne este E-mail, promovendo, desde logo, a eliminação do seu conteúdo em sua base de dados, registros ou sistema de controle. Fica desprovida de eficácia e validade a mensagem que contiver vínculos obrigacionais, expedida por quem não detenha poderes de representação.</span>"+			"</div>"+
+"</div>"+
+"</div>";
+
+
+var rodape_email_t = "<br><br><span>A E-Investor é uma plataforma desenvolvida pela Innvicton Tecnologia, que desenvolve softwares e tecnologias quantitativas para usuários investidores. A Innvicton Tecnologia é uma empresa de desenvolvimento de software e não provê qualquer tipo de serviço de investimento ou corretagem nos mercados financeiros. Sistema desenvolvido exclusivamente para integração ao testador de estratégias da Metaquotes para geração de parâmetros de algoritmo pelo usuário com auxílio de Inteligência Artificial. Não atuamos como prestadora de informações de mercado, ou como receptora/transmissora de ordens de negociação para o sistema de distribuição de valores mobiliários. Esta plataforma, serviços e sistemas não constitui nem deve ser interpretado como oferta ou solicitação de compra ou venda de qualquer instrumento financeiro, ficando a decisão de investimento sempre a critério exclusivo do usuário com base em seu juízo de valor. O investidor deve consultar seu próprio assessor ou conselheiro jurídico, tributário, regulatório, técnico de negócios, de investimentos, financeiro e contábil, na medida que julgar necessário, para assessoria na escolha de definição de softwares, plataformas, sistemas, algoritmos, estratégias e setups. O investidor que realiza operações de renda variável é o único responsável pelas decisões de investimento ou de abstenção de investimento que tomar.</span>"+
+"<br><br><span>Os sistemas da E-Investor são totalmente parametrizáveis (White Box), contendo seus parâmetros abertos para preenchimento pelo usuário da maneira que ele preferir e julgar mais adequada, cabendo a cada usuário a tomada de decisão da estratégia ou setup que vai utilizar. Comercialização de estratégias automatizadas; CNAE 6202-3/00; Regulatório CVM referente a Instrução CVM n° 598/18 conforme Item 15 Do Ofício-Circular CVM/SIN 02/19. Copyright - Innvicton Tecnologia - Todos os direitos reservados.</span>"+
+"<br><br><span>Empresas e tecnologias as quais utilizamos ou recomendamos não tem nenhuma ligação ou participação em qualquer negócio, produto ou serviço da E-Investor, e não obstante não tem ligação com este site, sistema ou conteúdo de nenhuma maneira comercial ou explícita.</span>"+
+"<br><br><span>AVISO LEGAL </span>"+
+"<br><span>Esta mensagem é destinada exclusivamente para a(s) pessoa(s) a quem é dirigida, podendo conter informação confidencial e/ou legalmente privilegiada. Desde já fica notificado de abster-se a divulgar, copiar, distribuir, ou, de qualquer forma, utilizar a informação contida nesta mensagem, por ser ilegal. Caso você tenha recebido esta mensagem por engano, pedimos que nos retorne este E-mail, promovendo, desde logo, a eliminação do seu conteúdo em sua base de dados, registros ou sistema de controle. Fica desprovida de eficácia e validade a mensagem que contiver vínculos obrigacionais, expedida por quem não detenha poderes de representação.</span>"+			"</div>"+
+"</div>"+
+"</div>";
+
 router.get('/', function(req, res, next) {
 
 	data.link_sistema = '/sistema';
@@ -255,19 +283,56 @@ router.post('/desativar_minha_conta', function(req, res, next) {
 	console.log(POST);
 	console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
 
+	usuarioStatusModel.find({id_usuario:mongoose.Types.ObjectId(req.session.usuario.id)},function(err,data_usuario_status_del){
 
-	const novo_desativar_conta = new desativarContaModel({ 
-		id_usuario:mongoose.Types.ObjectId(req.session.usuario.id),
-		desativar:true
-	});
+		console.log("data_usuario_status_del");
+		console.log(data_usuario_status_del);
 
-	novo_desativar_conta.save(function (err) {
-		if (err) {
-			return handleError(err);
-		}else{
-			res.json(data);
-		}
-	});
+
+		usuarioModel.find({'_id':mongoose.Types.ObjectId(req.session.usuario.id)},function(err,data_usuario_del){
+			console.log("data_usuario_del");
+			console.log(data_usuario_del);
+
+
+			const novo_desativar_conta = new desativarContaModel({ 
+				id_usuario:mongoose.Types.ObjectId(req.session.usuario.id),
+				desativar:true
+			});
+
+
+			var titulo = 'Desativação E-Investor';
+
+			var html = cabecalho_email +
+			"Olá " + data_usuario_del[0].nome + ", lamentamos que você queira desativar seu plano da licença  " + data_usuario_status_del[0].nome_algoritmo_escolhido  +". Todos os seus dados e acessos serão excluídos, bem como todas integrações feitas serão desativadas. <br>Essa ação é irreversível."+
+			"<br><br>Caso possamos fazer algo para que você continue utilizando o sistema, ou tenha ocorrido algum problema, estamos a disposição para lhe atender e ajudar."+
+			"<br><br>Atenciosamente,<br>Equipe E-Investor"+
+			"<br><br><span style='font-size:9px;'>Algo errado? Entre em contato conosco respondendo este e-mail.</span>"+
+			rodape_email;
+
+
+			var text = "<b>E-Investor</b>"+
+			"Olá " + data_usuario_del[0].nome + ", lamentamos que você queira desativar seu plano da licença  " + data_usuario_status_del[0].nome_algoritmo_escolhido  +". Todos os seus dados e acessos serão excluídos, bem como todas integrações feitas serão desativadas. <br>Essa ação é irreversível."  +
+			"<br><br>Caso possamos fazer algo para que você continue utilizando o sistema, ou tenha ocorrido algum problema, estamos a disposição para lhe atender e ajudar."+
+			"<br><br>Atenciosamente,<br>Equipe E-Investor"+
+			"<br><br><span>Algo errado? Entre em contato conosco respondendo este e-mail.</span>"+ rodape_email_t;
+
+
+			control.SendMail(data_usuario_del[0].email, titulo ,text,html);
+			control.SendMail('suporte@einvestor.com.br', titulo ,text,html);
+
+
+
+			novo_desativar_conta.save(function (err) {
+				if (err) {
+					return handleError(err);
+				}else{
+					res.json(data);
+				}
+			});
+
+		}).sort({'_id':-1}).limit(1);
+
+	}).sort({'_id':-1}).limit(1);
 
 
 
